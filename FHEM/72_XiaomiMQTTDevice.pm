469236b518f58722c8c117586c18c5dc13ce8ee3
Oskar Neumann <oskar.neumann@me.com>
2018-05-30 20:50:14 +0200

    compatibility with @Koenkk/zigbee2mqtt

--- FHEM/72_XiaomiMQTTDevice.pm
+++ FHEM/72_XiaomiMQTTDevice.pm
@@ -18,7 +18,7 @@ sub XiaomiMQTTDevice_Initialize($) {
     $hash->{UndefFn} = "XiaomiMQTT::DEVICE::Undefine";
     $hash->{SetFn} = "XiaomiMQTT::DEVICE::Set";
     #$hash->{GetFn} = "XiaomiMQTT::DEVICE::Get";
-    $hash->{AttrFn} = "MQTT::DEVICE::Attr";
+    $hash->{AttrFn} = "XiaomiMQTT::DEVICE::Attr";
     $hash->{AttrList} = "IODev qos retain " . $main::readingFnAttributes;
     $hash->{OnMessageFn} = "XiaomiMQTT::DEVICE::onmessage";
 
@@ -74,33 +74,27 @@ sub Define() {
     $hash->{TYPE} = $type;
     $main::modules{XiaomiMQTTDevice}{defptr}{$id} = $hash;
 
-    # Subscribe Readings
-    my $newTopic = XiaomiMQTT::DEVICE::GetTopicFor($hash) . '#';
-    my ($mqos, $mretain, $mtopic, $mvalue, $mcmd) = MQTT::parsePublishCmdStr($newTopic);
-    client_subscribe_topic($hash, $mtopic, $mqos, $mretain);
-    Log3($hash->{NAME}, 5, "automatically subscribed to topic: " . $newTopic);
-
     if($hash->{MODEL} eq 'bridge') {
-      main::InternalTimer(main::gettimeofday()+2, "XiaomiMQTT::DEVICE::updateDevices", $hash, 1);
+        main::InternalTimer(main::gettimeofday()+2, "XiaomiMQTT::DEVICE::updateDevices", $hash, 1);
     }
 
     if (AttrVal($name, "stateFormat", "transmission-state") eq "transmission-state") {
-        if ( $model eq 'sensor_ht') {
+        if ( $model =~ m/WSDCGQ01LM/) {
             $main::attr{$name}{stateFormat}  = 'temperature °C, humidity %';
         }
-        elsif ( $model eq 'weather') {
+        elsif ( $model =~ m/WSDCGQ11LM/) {
             $main::attr{$name}{stateFormat}  = 'temperature °C, humidity %, pressure kPa';
-        } elsif($model ne 'unknown') {
+        } elsif($model =~ m/unknown/) {
             $main::attr{$name}{stateFormat} = "state";
         }
         $hash->{STATE} = "paired";
     }
 
     if(!defined($main::attr{$name}{devStateIcon})) {
-        if( $model =~ /motion/) {
+        if( $model =~ m/(RTCGQ11LM|RTCGQ01LM|motion)/ ) {
             $main::attr{$name}{devStateIcon}  = 'motion:motion_detector@red off:motion_detector@green no_motion:motion_detector@green' ;
         }
-        elsif ( $model =~ /magnet/) {
+        elsif ( $model =~ m/(MCCGQ11LM|MCCGQ01LM|magnet)/ ) {
             $main::attr{$name}{devStateIcon}  = 'open:fts_door_open@red close:fts_door@green';
         }
     }
@@ -108,20 +102,43 @@ sub Define() {
     return undef;
 };
 
+sub Attr($$$$) {
+    my ($command, $name, $attribute, $value) = @_;
+    my $hash = $defs{$name};
+
+    my $result = MQTT::DEVICE::Attr($command, $name, $attribute, $value);
+
+    if ($attribute eq "IODev") {
+        # Subscribe Readings
+        my ($mqos, $mretain, $mtopic, $mvalue, $mcmd) = MQTT::parsePublishCmdStr(XiaomiMQTT::DEVICE::GetTopicFor($hash));
+        client_subscribe_topic($hash, $mtopic, $mqos, $mretain);
+
+        ($mqos, $mretain, $mtopic, $mvalue, $mcmd) = MQTT::parsePublishCmdStr('xiaomi/'. $hash->{SID}. '/#');
+        client_subscribe_topic($hash, $mtopic, $mqos, $mretain);
+
+        if($hash->{MODEL} eq 'bridge') {
+            ($mqos, $mretain, $mtopic, $mvalue, $mcmd) = MQTT::parsePublishCmdStr("zigbee2mqtt/bridge/log");
+            client_subscribe_topic($hash, $mtopic, $mqos, $mretain);
+        }
+    }
+
+    return $result;
+}
+
+
 sub GetTopicFor {
-    my ($hash, $cmnd) = @_;
-    return 'xiaomi/'. (defined $cmnd ? 'cmnd/' : '') . $hash->{SID} . '/' . (defined $cmnd ? $cmnd : '');
+    my ($hash) = @_;
+    return "zigbee2mqtt/" . ($hash->{SID});
 }
 
 sub Undefine($$) {
     my ($hash, $name) = @_;
 
-    my $oldTopic = XiaomiMQTT::DEVICE::GetTopicFor($hash) . '#';
-    client_unsubscribe_topic($hash, $oldTopic);
+    client_unsubscribe_topic($hash, XiaomiMQTT::DEVICE::GetTopicFor($hash));
+    client_unsubscribe_topic($hash, 'xiaomi/'. $hash->{SID}. '/#');
+    client_unsubscribe_topic($hash, 'zigbee2mqtt/bridge/log') if($hash->{MODEL} eq 'bridge');
+
     delete($main::modules{XiaomiMQTTDevice}{defptr}{$hash->{SID}});
-
-    Log3($hash->{NAME}, 5, "automatically unsubscribed from topic: " . $oldTopic);
-
     return MQTT::Client_Undefine($hash);
 }
 
@@ -131,9 +148,7 @@ sub Set($$$@) {
     if ($command eq '?') {
     	 my $cmdList = "";
 	    if ($hash->{MODEL} eq "bridge") {
-	    	$cmdList = "pairForSec updateDevices";
-	    } else {
-	    	$cmdList = "unpair:noArg";
+	    	$cmdList = "pair:1,0 updateDevices:noArg";
 	    }
         return "Unknown argument " . $command . ", choose one of ". $cmdList;
     }
@@ -146,27 +161,19 @@ sub Set($$$@) {
     my $qos = $hash->{".qos"}->{'*'};
 
     if ($hash->{MODEL} eq "bridge") {
-        if($command eq 'pairForSec') {
-            my $topic = XiaomiMQTT::DEVICE::GetTopicFor($hash, 'pair');
+        if($command eq 'pair' || $command eq 'pairForSec') {
             my $value = join (" ", @values);
 
-            $msgid = send_publish($hash->{IODev}, topic => $topic, message => $value, qos => $qos, retain => $retain);
-
-            Log3($hash->{NAME}, 5, "sent (cmnd) '" . $value . "' to " . $topic);
+            $msgid = send_publish($hash->{IODev}, topic => 'zigbee2mqtt/bridge/config/permit_join', message => $value == 0 ? "false" : "true", qos => $qos, retain => $retain);
+            $msgid = send_publish($hash->{IODev}, topic => 'xiaomi/cmnd/bridge/pair', message => 220, qos => $qos, retain => $retain); #backwards compatibility
+            main::RemoveInternalTimer($hash);
+            main::InternalTimer(main::gettimeofday()+5*60, "XiaomiMQTT::DEVICE::endPairing", $hash, 1);
         }
 
         if ($command eq "updateDevices") {
             updateDevices($hash);
         }
-    } else {
-        if($command eq 'unpair') {
-            my $topic = XiaomiMQTT::DEVICE::GetTopicFor($hash, 'unpair');
-            $msgid = send_publish($hash->{IODev}, topic => $topic, message => "", qos => $qos, retain => $retain);
-
-            Log3($hash->{NAME}, 5, "sent (cmnd) '' to " . $topic);
-        }
     }
-
 
 
     $hash->{message_ids}->{$msgid}++ if defined $msgid;
@@ -185,7 +192,39 @@ sub onmessage($$$) {
 
     Log3($hash->{NAME}, 5, "received message '" . $message . "' for topic: " . $topic);
     my @parts = split('/', $topic);
-    if($parts[-2] eq $hash->{SID}) {
+    my $path = $parts[-1];
+
+    if($topic =~ m/bridge\/log/) {
+        my $name = $hash->{NAME};
+        my $json = eval { JSON->new->utf8(0)->decode($message) };
+        if($json->{type} eq "devices") {
+            foreach my $device (@{$json->{message}}) {
+              my $sid = $device->{ieeeAddr};
+              my $model = $device->{model};
+              $model = 'unknown' if(!defined $model);
+              if (!defined $main::modules{XiaomiMQTTDevice}{defptr}{$sid}) {
+                Log3 $name, 4, "$name: DEV_Parse> UNDEFINED " . $model . " : " .$sid;
+                main::DoTrigger("global", "UNDEFINED XMI_$sid XiaomiMQTTDevice $model $sid");
+              } else {
+                my $defined = $main::modules{XiaomiMQTTDevice}{defptr}{$sid};
+                if($defined->{model} ne $model) {
+                    fhem('modify '. $defined->{NAME} . ' '. $model . ' '. $sid);
+                }
+              }
+            }
+
+            main::CommandSave(undef, undef);
+            return
+        }
+
+        if($json->{type} eq "device_connected") {
+            updateDevices($hash);
+        }
+    }
+
+    if($parts[-1] eq $hash->{SID}) {
+        XiaomiMQTT::DEVICE::Decode($hash, $message);
+    } elsif($parts[-2] eq $hash->{SID} && $parts[0] eq "xiaomi") { #backward compatibility, not needed with new fork
         my $path = $parts[-1];
 
         if($path eq 'devices') {
@@ -204,12 +243,6 @@ sub onmessage($$$) {
             return;
         }
 
-        if($path eq 'unpair') {
-            fhem('delete '. $hash->{NAME});
-            main::CommandSave(undef, undef);
-            return;
-        }
-
         if($path eq 'model' && $message =~ m/[A-Za-z]/) {
             return if($message eq $hash->{MODEL});
             my @args = split("[ \t]+", $hash->{DEF});
@@ -224,16 +257,14 @@ sub onmessage($$$) {
         }
 
         readingsSingleUpdate($hash, $path, $message, 1);
-    }
-
-    else {
+    } else {
       # Forward to "normal" logic
         MQTT::DEVICE::onmessage($hash, $topic, $message);
     }
 }
 
-sub Decode($$$) {
-    my ($hash, $reading, $value) = @_;
+sub Decode($$) {
+    my ($hash, $value) = @_;
     my $h;
 
     eval {
@@ -242,18 +273,18 @@ sub Decode($$$) {
     };
 
     if ($@) {
-        Log3($hash->{NAME}, 2, "bad JSON: $reading: $value - $@");
+        Log3($hash->{NAME}, 2, "bad JSON: $value - $@");
         return undef;
     }
 
     readingsBeginUpdate($hash);
-    XiaomiMQTT::DEVICE::Expand($hash, $h, $reading . "-", "");
+    XiaomiMQTT::DEVICE::Expand($hash, $h, "", "");
     readingsEndUpdate($hash, 1);
 
     return undef;
 }
 
-sub Expand($$$$) {
+sub Expand {
     my ($hash, $ref, $prefix, $suffix) = @_;
 
     $prefix = "" if (!$prefix);
@@ -266,11 +297,36 @@ sub Expand($$$$) {
         }
     } elsif (ref($ref) eq "HASH") {
         while (my ($key, $value) = each %{$ref}) {
-            if (ref($value)) {
+            if (ref($value) && ref($value) ne "JSON::XS::Boolean") {
                 XiaomiMQTT::DEVICE::Expand($hash, $value, $prefix . $key . $suffix . "-", "");
             } else {
                 # replace illegal characters in reading names
                 (my $reading = $prefix . $key . $suffix) =~ s/[^A-Za-z\d_\.\-\/]/_/g;
+                if(ref($value) eq "JSON::XS::Boolean") {
+                    $value = $value ? "true" : "false";
+                }
+                if($reading eq 'battery') {
+                    readingsBulkUpdate($hash, 'battery_level', $value);
+                    $value = $value < 80 ? 'low' : 'ok';
+                }
+                if($reading eq 'occupancy') {
+                    readingsBulkUpdate($hash, 'state', $value eq "true" ? 'motion' : 'no_motion');
+                }
+                if($reading eq 'contact') {
+                    readingsBulkUpdate($hash, 'state', $value eq "true" ? 'open' : 'close');
+                }
+                if($reading eq 'illuminance') {
+                    readingsBulkUpdate($hash, 'lux', $value);
+                }
+                if($reading eq 'click') {
+                    my $previousValue = $value;
+                    $value = 'click_release' if($value eq 'single');
+                    $value = 'double_click' if($value eq 'double');
+                    $value = 'long_click_release' if($value eq 'long_release');
+                    $value = $value . '_click' if($value eq $previousValue);
+                    readingsBulkUpdate($hash, 'state', $value);
+                    $value = $previousValue;
+                }
                 readingsBulkUpdate($hash, lc($reading), $value);
             }
         }
@@ -279,13 +335,17 @@ sub Expand($$$$) {
 
 sub updateDevices($) {
 	my ($hash) = @_;
-    my $topic = XiaomiMQTT::DEVICE::GetTopicFor($hash, 'getDevices');
-
     my $retain = $hash->{".retain"}->{'*'};
     my $qos = $hash->{".qos"}->{'*'};
-    my $msgid = send_publish($hash->{IODev}, topic => $topic, message => "", qos => $qos, retain => $retain);
+    my $msgid = send_publish($hash->{IODev}, topic => "zigbee2mqtt/bridge/config/devices", message => "", qos => $qos, retain => $retain);
+    $msgid = send_publish($hash->{IODev}, topic => 'xiaomi/cmnd/bridge/getDevices', message => "", qos => $qos, retain => $retain); #backwards compatibility
+}
 
-    Log3($hash->{NAME}, 5, "sent (cmnd) '' to " . $topic);
+sub endPairing {
+    my ($hash) = @_;
+    my $retain = $hash->{".retain"}->{'*'};
+    my $qos = $hash->{".qos"}->{'*'};
+    my $msgid = send_publish($hash->{IODev}, topic => "zigbee2mqtt/bridge/config/permit_join", message => "false", qos => $qos, retain => $retain); 
 }
 
 1;
